7802223039455461c7da0567d86edd68d050b951
Jon Kalfayan <jonk1993@gmail.com>
2014-12-04 16:12:42 -0800

    So it works, but I'm not using background pages to keep it persistent. I need to use background pages

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-function clickHandler(e) {
-  setTimeout(alert, 1000);
-  setTimeout(addSomething, 1000);
+var timeLeft = 0;
+
+function updateTimer(time) {
+    timer = document.getElementById('timer');
+    timer.innerHTML = time + ":00";
+
+    timeLeft = time * 60;
 }
 
-function addSomething() {
-    console.log("ay");
-    document.getElementById('demo').innerHTML = "helloWorld!"
+function handleTimer() {
+    setInterval(function() {startTiming()}, 1000);
+
 }
 
-function alert() {
-    alert('hay!')
+function startTiming() {
+    timeLeft = timeLeft - 1;
+    document.getElementById('timer').innerHTML = Math.floor(timeLeft / 60) + ":" + timeLeft % 60
 }
 
 function main() {
-  // Initialization work goes here.
+
+    document.getElementById('15_min').addEventListener('click', function() { updateTimer(15) });
+    document.getElementById('30_min').addEventListener('click', function() { updateTimer(30) });
+    document.getElementById('60_min').addEventListener('click', function() { updateTimer(60) });
+    document.getElementById('start_button').addEventListener('click', handleTimer);
+
 }
 
 // Add event listeners once the DOM has fully loaded by listening for the
 // `DOMContentLoaded` event on the document, and adding your listeners to
 // specific elements when it triggers.
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', addSomething);
-  main();
+    main();
 });
